248fec4d7b2229c74306028658a9e2c9cda619e8
genggeng538 <214751817@qq.com>
2026-02-04 10:09:00 +0800

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,8 @@
+# config/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    # 登录/退出：用 Django 自带的，最稳定
-    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-
-    # 主页给 ERP app
     path("", include("erp.urls")),
 ]
