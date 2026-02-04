8ab5419b55d6eb0d11cb6d8eafb99719f4e659f3
genggeng538 <214751817@qq.com>
2026-02-04 09:32:36 +0800

    Update urls.py

--- erp/urls.py
+++ erp/urls.py
@@ -1,6 +1,9 @@
+# erp/urls.py
 from django.urls import path
-from .views import dashboard
+from .views import login_view, logout_view, dashboard
 
 urlpatterns = [
     path("", dashboard, name="dashboard"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
