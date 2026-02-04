d405f2074a73de2d1f61b6246b5a81becc7ff4bc
genggeng538 <214751817@qq.com>
2026-02-04 08:51:11 +0800

    Update admin.py

--- erp/admin.py
+++ erp/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-from .models import SalesOrder, WorkOrder
+from .models import SalesOrder
+
 
 @admin.register(SalesOrder)
 class SalesOrderAdmin(admin.ModelAdmin):
-    list_display = ("so_no", "customer", "dn", "qty", "created_at")
-    search_fields = ("so_no", "customer")
-
-@admin.register(WorkOrder)
-class WorkOrderAdmin(admin.ModelAdmin):
-    list_display = ("sales_order", "process", "status", "finished_at")
-    list_filter = ("status", "process")
+    list_display = ("order_no", "customer_name", "created_at")
+    search_fields = ("order_no", "customer_name")
