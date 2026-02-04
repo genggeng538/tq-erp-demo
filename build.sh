81264845d4577584bde43e9212ccf5f550c8a573
genggeng538 <214751817@qq.com>
2026-02-04 08:24:02 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-
-python manage.py collectstatic --noinput
+echo "==> Running migrations"
 python manage.py migrate --noinput
+
+echo "==> Collecting static files"
+python manage.py collectstatic --noinput
