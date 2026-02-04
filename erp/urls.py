523e7d4cc58f84b9046be471e09b7645ec74381c
genggeng538 <214751817@qq.com>
2026-02-04 09:19:06 +0800

    Update urls.py

--- erp/urls.py
+++ erp/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from . import views
+from .views import dashboard
 
 urlpatterns = [
-    path("sales/", views.sales_order_list, name="sales_list"),
-    path("sales/new/", views.sales_order_new, name="sales_new"),
-    path("work/", views.work_order_list, name="work_list"),
-    path("work/new/", views.work_order_new, name="work_new"),
+    path("", dashboard, name="dashboard"),
 ]
