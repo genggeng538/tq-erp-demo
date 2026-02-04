3e01ddf6d4a55a8f8ea7a57290847a9a993d7dac
genggeng538 <214751817@qq.com>
2026-02-04 10:49:50 +0800

    Update views.py

--- erp/views.py
+++ erp/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_protect
 
+from django.db import models
 from .models import SalesOrder, WorkOrder, InventoryItem
 
 
